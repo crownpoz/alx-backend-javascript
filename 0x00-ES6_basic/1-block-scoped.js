0106933f280f3c4c949a2ee1e48a0b126f915d0c
crownpoz <agbeyoadebimpe@gmail.com>
2023-01-24 18:47:17 +0100

    update

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,12 +1,12 @@
 export default function taskBlock(trueOrFalse) {
-  var task = false;
-  var task2 = true;
+  const task = false;
+  const task2 = true;
 
   if (trueOrFalse) {
     // const would not change in code block
     const task = true;
     // const would not change in code block
-    var task2 = false;
+    const task2 = false;
   }
 
   return [task, task2];
