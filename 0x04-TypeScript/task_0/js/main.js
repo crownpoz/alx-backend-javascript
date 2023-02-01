b0f752670d95e29bb358799e19ec4c01f6114383
crownpoz <agbeyoadebimpe@gmail.com>
2023-02-01 19:10:11 +0100

    interface

---
+++ 0x04-TypeScript/task_0/js/main.js
@@ -0,0 +1,12 @@
+const student1 = {
+    firstName: 'Benny',
+    lastName: 'Francis',
+    age: 29,
+    location: 'Lagos',
+};
+const student2 = {
+    firstName: 'Uwaila',
+    lastName: 'Akpan',
+    age: 30,
+    location: 'Akwa Ibom',
+};
---
+++ 0x04-TypeScript/task_0/js/main.ts
@@ -0,0 +1,20 @@
+interface Student {
+    firstName: string;
+    lastName: string;
+    age: number;
+    location: string;
+}
+
+const student1: Student = {
+    firstName: 'Benny',
+    lastName: 'Francis',
+    age: 29,
+    location: 'Lagos',
+}
+
+const student2: Student = {
+    firstName: 'Uwaila',
+    lastName: 'Akpan',
+    age: 30,
+    location: 'Akwa Ibom',
+}
