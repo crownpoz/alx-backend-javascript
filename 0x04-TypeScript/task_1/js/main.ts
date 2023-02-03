77d552c7f607ef57c51be66663ae4796048decb7
crownpoz <agbeyoadebimpe@gmail.com>
2023-02-03 14:29:21 +0100

    typescript

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -1,8 +1,47 @@
 interface Teacher {
-  readonly firstName: string;
-  readonly lastName: string;
-  fullTimeEmployee: boolean;
-  yearsOfExperience?: number;
-  location: string;
-  [propName: string]: any;
+    readonly firstName: string;
+    readonly lastName: string;
+    fullTimeEmployee: boolean;
+    yearsOfExperience?: number;
+    location: string;
+    [propName: string]: any;
+}
+
+interface Director extends Teacher {
+    numberOfReports: number;
+}
+
+interface printTeacherFunction{
+    (firstName : string, lastName: string): string;
+}
+
+const printTeacher: printTeacherFunction = function (firstname: string, lastname: string) :string;{
+    return `${firstname.charAt(0)}. ${lastName}`;
+}
+
+interface studentClassInterface {
+    workOnHomework() : string;
+    displayName() : string;
+}
+
+class StudentClass implements studentClassInterface {
+    firstName: string;
+    lastName: string;
+
+    constructor(firstName: string, lastName: string) {
+        this.firstName = firstName;
+        this.lastname = lastName;
+    }
+
+    workOnHomework() : string;{
+        return 'Currently working'
+    }
+
+    displayName() : string;{
+        return this.firstName;
+    }
+}
+
+interface studentClassConstructor {
+    (firstName: string, lastName: string): studentClassInterface;
 }
